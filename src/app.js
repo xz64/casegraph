c7e2d331e88c09fb4d3b06d1f702bc1e820c7e7d
S K <xz64@users.noreply.github.com>
2016-04-24 19:44:32 -0400

    remove normalize css
    This was messing up tooltip positioning.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 'use strict';
-require('normalize.css');
 require('./styles/main.css');
 var $ = require('jquery');
 var _ = require('lodash');
@@ -11,26 +10,31 @@ var csvReader = require('./helpers/csv-reader.js');
 
 var buckets = [0, 1, 2, 4, 7, 14, 30, 90];
 
-function resizeGraph() {
-  graph.resize();
-}
-
-function onFileData(err, data) {
-  var owners = dataUtils.getOwners(data).sort();
-  var graphData = dataUtils.getGraphData(data, buckets, 'createddate');
-  var categories = dataUtils.getBuckets(graphData);
-  var graphElement = $('#graph')[0];
-  graph.init(graphElement);
-  dropdown.render(document.getElementById('filterList'), owners, graphData);
-  graph.render(graphData, categories, 'createddate');
-  $(window).on('resize', _.debounce(resizeGraph, 200));
-}
+var app = {
+  resizeGraph: function resizeGraph() {
+    graph.resize();
+  },
+  graphData: function graphData(data, metric) {
+  },
+  onFileData: function onFileData(err, data) {
+    this.data = data;
+    this.owners = dataUtils.getOwners(this.data).sort();
+    this.graphData = dataUtils.getGraphData(this.data, buckets, 'createddate');
+    this.categories = dataUtils.getBuckets(this.graphData);
+    var graphElement = $('#graph')[0];
+    graph.init(graphElement);
+    dropdown.render(document.getElementById('filterList'), this.owners,
+      this.graphData);
+    graph.render(this.graphData, this.categories, 'createddate');
+    $(window).on('resize', _.debounce(this.resizeGraph.bind(this), 200));
+  }
+};
 
 $(document).ready(function domready() {
   document.body.innerHTML = mainPage();
 
   $('#file_upload').on('click', function() {
     var file = $('#file')[0].files[0];
-    csvReader.readFile(file, onFileData);
+    csvReader.readFile(file, app.onFileData.bind(app));
   });
 });
